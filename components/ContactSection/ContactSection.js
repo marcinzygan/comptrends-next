bd3cc8a178fa4bccce9f5764e6954c37241ea61b
marcinzygan <soul@o2.pl>
2022-12-14 11:01:36 +0100

    added styles for contact section up to 1300px

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -30,26 +30,33 @@ const ContactSection = () => {
         </div>
       </div>
       <div className={classes.contact__container}>
-        <div className={classes.contact__details}>
-          <img
-            className={classes.contact__img}
-            src="/about.png"
-            alt="phone email details"
-          ></img>
-          <div
-            className={"social__icons" + " " + classes.social__icons_contact}
-          >
-            <Icon icon="entypo-social:facebook-with-circle" />
-            <Icon icon="entypo-social:linkedin-with-circle" />
-            <Icon icon="entypo-social:twitter-with-circle" />
-          </div>
-          <p className={classes.contact__details_txt}>
-            Phone : +48 001 001 001
-          </p>
-          <p className={classes.contact__details_txt}>
-            Email : comptrends@gmail.com
-          </p>
+        {/* <div className={classes.contact__details}> */}
+        <img
+          className={classes.contact__img}
+          src="/about.png"
+          alt="phone email details"
+        ></img>
+        <div className={"social__icons" + " " + classes.social__icons_contact}>
+          <Icon icon="entypo-social:facebook-with-circle" />
+          <Icon icon="entypo-social:linkedin-with-circle" />
+          <Icon icon="entypo-social:twitter-with-circle" />
         </div>
+        <p
+          className={
+            classes.contact__details_txt + " " + classes.contact__details_phone
+          }
+        >
+          Phone<span className="span__accentFirst"> : </span> +48 001 001 001
+        </p>
+        <p
+          className={
+            classes.contact__details_txt + " " + classes.contact__details_email
+          }
+        >
+          Email<span className="span__accentFirst"> : </span>
+          comptrends@gmail.com
+        </p>
+        {/* </div> */}
         <Form />
       </div>
     </section>
