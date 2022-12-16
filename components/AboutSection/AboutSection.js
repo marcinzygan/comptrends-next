82f4e1cf231d77ff19201f893e2142e7365eac01
marcinzygan <soul@o2.pl>
2022-12-16 10:22:37 +0100

    added styles for offer section 800px

--- components/AboutSection/AboutSection.js
+++ components/AboutSection/AboutSection.js
@@ -6,10 +6,13 @@ import classes from "./AboutSection.module.css";
 const AboutSection = () => {
   return (
     <section id="about">
-      <SectionHeading heading={"Who we are ?"} />
+      <SectionHeading
+        heading={"Who we are ?"}
+        subHeading={"Data is our passion "}
+      />
       <div className={classes.about__container}>
-        <div className={"about__txt_container"}>
-          <h2 className={"h2__txt"}>
+        <div className={classes.about__txt_container}>
+          <h2 className={"h2__txt" + " " + classes.about__h2}>
             <span className={"span__accentSecond"}>Data</span> engineering and
             web development company
           </h2>
@@ -23,9 +26,34 @@ const AboutSection = () => {
             Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
             dui. Elit risus morbi etiam diam tempus.{" "}
           </p>
+          <img
+            src="/about.png"
+            alt="robot showing graph on coputer screen"
+            className={classes.about__img}
+          ></img>
         </div>
-        <img src="/about.png" alt="robot showing graph on coputer screen"></img>
-        <div className={"btn"}>
+
+        <div className={classes.about__txt_container}>
+          <div>
+            <p className={"paragraph__txt"}>
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus. Vitae amet metus ante
+              gravida nisi eget imperdiet. Consectetur ut lorem mattis felis
+              sapien diam arcu
+            </p>
+            <p className={"paragraph__txt"}>
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus.{" "}
+            </p>
+          </div>
+          <img
+            src="/about.png"
+            alt="robot showing graph on coputer screen"
+            className={classes.about__img}
+          ></img>
+        </div>
+
+        <div className={"btn" + " " + classes.about__btn}>
           <Link href="/#offer" scroll={false}>
             Our offer
           </Link>
--- components/OfferSection/OfferSection.js
+++ components/OfferSection/OfferSection.js
@@ -30,64 +30,66 @@ const OfferSection = () => {
             classes.offer__info_container + " " + classes.offer__info_help
           }
         >
-          <motion.h2
-            className="h2__txt"
-            initial={"hidden"}
-            whileInView={"visible"}
-            viewport={{ once: false }}
-            variants={{
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 1, delay: 0.5 },
-              },
-              hidden: { opacity: 0, y: -100 },
-            }}
-          >
-            <span className="span__accentFirst">Help</span> to promote your
-            business
-          </motion.h2>
-          <p className="paragraph__txt">
-            Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
-            dui. Elit risus morbi etiam diam tempus. Vitae amet metus ante
-            gravida nisi eget imperdiet. Consectetur ut lorem mattis felis
-            sapien diam arcu
-          </p>
-          <div
-            onClick={toggleHelp}
-            className={classes.offer__btn + " " + classes.help__btn}
-          >
-            {isHelpSHown ? "Read Less" : "Read More"}
-            {/* <Icon
+          <div>
+            <motion.h2
+              className="h2__txt"
+              initial={"hidden"}
+              whileInView={"visible"}
+              viewport={{ once: false }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, delay: 0.5 },
+                },
+                hidden: { opacity: 0, y: -100 },
+              }}
+            >
+              <span className="span__accentFirst">Help</span> to promote your
+              business
+            </motion.h2>
+            <p className="paragraph__txt">
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus. Vitae amet metus ante
+              gravida nisi eget imperdiet. Consectetur ut lorem mattis felis
+              sapien diam arcu
+            </p>
+            <div
+              onClick={toggleHelp}
+              className={classes.offer__btn + " " + classes.help__btn}
+            >
+              {isHelpSHown ? "Read Less" : "Read More"}
+              {/* <Icon
              icon="ph:magnifying-glass-light"
               className={classes.icon__read}
             />  */}
+            </div>
+            <motion.p
+              initial={isHelpSHown ? { opacity: 0 } : { opacity: 1 }}
+              animate={isHelpSHown ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ duration: 2, ease: "easeOut" }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 1, ease: "easeOut" },
+              }}
+              className={"paragraph__txt" + " " + classes.offer__more_txt}
+            >
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus. Lorem ipsum dolor sit
+              amet consectetur. Vitae odio quis sit velit dui. Elit risus morbi
+              etiam diam tempus. Lorem ipsum dolor sit amet consectetur. Vitae
+              odio quis sit velit dui. Elit risus morbi etiam diam tempus. Lorem
+              ipsum dolor sit amet consectetur. Vitae odio quis sit velit dui.
+              Elit risus morbi etiam diam tempus. diam tempus. Lorem ipsum dolor
+              sit amet consectetur. Vitae odio quis sit velit dui. Elit risus
+              morbi etiam diam tempus. Lorem ipsum dolor sit amet consectetur.
+              Vitae odio quis sit velit dui. Elit risus morbi etiam diam tempus.
+              diam tempus. Lorem ipsum dolor sit amet consectetur. Vitae odio
+              quis sit velit dui. Elit risus morbi etiam diam tempus. Lorem
+              ipsum dolor sit amet consectetur. Vitae odio quis sit velit dui.
+              Elit risus morbi etiam diam tempus.
+            </motion.p>{" "}
           </div>
-          <motion.p
-            initial={isHelpSHown ? { opacity: 0 } : { opacity: 1 }}
-            animate={isHelpSHown ? { opacity: 1 } : { opacity: 0 }}
-            transition={{ duration: 2, ease: "easeOut" }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 1, ease: "easeOut" },
-            }}
-            className={"paragraph__txt" + " " + classes.offer__more_txt}
-          >
-            Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
-            dui. Elit risus morbi etiam diam tempus. Lorem ipsum dolor sit amet
-            consectetur. Vitae odio quis sit velit dui. Elit risus morbi etiam
-            diam tempus. Lorem ipsum dolor sit amet consectetur. Vitae odio quis
-            sit velit dui. Elit risus morbi etiam diam tempus. Lorem ipsum dolor
-            sit amet consectetur. Vitae odio quis sit velit dui. Elit risus
-            morbi etiam diam tempus. diam tempus. Lorem ipsum dolor sit amet
-            consectetur. Vitae odio quis sit velit dui. Elit risus morbi etiam
-            diam tempus. Lorem ipsum dolor sit amet consectetur. Vitae odio quis
-            sit velit dui. Elit risus morbi etiam diam tempus. diam tempus.
-            Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
-            dui. Elit risus morbi etiam diam tempus. Lorem ipsum dolor sit amet
-            consectetur. Vitae odio quis sit velit dui. Elit risus morbi etiam
-            diam tempus.
-          </motion.p>
 
           <motion.img
             src="/about.png"
@@ -178,58 +180,60 @@ const OfferSection = () => {
             classes.offer__info_container + " " + classes.offer__info_data
           }
         >
-          <motion.h2
-            className="h2__txt"
-            initial={"hidden"}
-            whileInView={"visible"}
-            viewport={{ once: false }}
-            variants={{
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { duration: 1, delay: 0.5 },
-              },
-              hidden: { opacity: 0, y: -100 },
-            }}
-          >
-            Bring your <span className="span__accentSecond">data</span> to live
-          </motion.h2>
-          <p className={"paragraph__txt" + " " + classes.offer__data_txt}>
-            Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
-            dui. Elit risus morbi etiam diam tempus. Vitae amet metus ante
-            gravida nisi eget imperdiet. Consectetur ut lorem mattis felis
-            sapien diam arcu
-          </p>
-          <div onClick={toggleData} className={classes.offer__btn}>
-            {isDataSHown ? "Read Less" : "Read More"}
-            {/* <Icon
+          <div>
+            <motion.h2
+              className="h2__txt"
+              initial={"hidden"}
+              whileInView={"visible"}
+              viewport={{ once: false }}
+              variants={{
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, delay: 0.5 },
+                },
+                hidden: { opacity: 0, y: -100 },
+              }}
+            >
+              Bring your <span className="span__accentSecond">data</span> to
+              live
+            </motion.h2>
+            <p className={"paragraph__txt" + " " + classes.offer__data_txt}>
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus. Vitae amet metus ante
+              gravida nisi eget imperdiet. Consectetur ut lorem mattis felis
+              sapien diam arcu
+            </p>
+            <div onClick={toggleData} className={classes.offer__btn}>
+              {isDataSHown ? "Read Less" : "Read More"}
+              {/* <Icon
               icon="ph:magnifying-glass-light"
               className={classes.icon__read}
             /> */}
+            </div>
+
+            <motion.p
+              initial={isDataSHown ? { opacity: 1 } : { opacity: 1 }}
+              animate={isDataSHown ? { opacity: 1 } : { opacity: 0 }}
+              transition={{ duration: 2, ease: "easeOut" }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 1, ease: "easeOut" },
+              }}
+              className={"paragraph__txt" + " " + classes.offer__more_txt}
+            >
+              Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
+              dui. Elit risus morbi etiam diam tempus.Lorem ipsum dolor sit amet
+              consectetur. Vitae odio quis sit velit dui.<br></br> Elit risus
+              morbi etiam diam tempus. Lorem ipsum dolor sit amet consectetur.
+              Vitae odio quis sit velit dui. Elit risus morbi etiam diam
+              tempus.Lorem ipsum dolor sit amet consectetur.
+              <br></br> Vitae odio quis sit velit dui. Elit risus morbi etiam
+              diam tempus. Lorem ipsum dolor sit amet consectetur. Vitae odio
+              quis sit velit dui. <br></br>
+              Elit risus morbi etiam diam tempus.
+            </motion.p>
           </div>
-
-          <motion.p
-            initial={isDataSHown ? { opacity: 1 } : { opacity: 1 }}
-            animate={isDataSHown ? { opacity: 1 } : { opacity: 0 }}
-            transition={{ duration: 2, ease: "easeOut" }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 1, ease: "easeOut" },
-            }}
-            className={"paragraph__txt" + " " + classes.offer__more_txt}
-          >
-            Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit velit
-            dui. Elit risus morbi etiam diam tempus.Lorem ipsum dolor sit amet
-            consectetur. Vitae odio quis sit velit dui.<br></br> Elit risus
-            morbi etiam diam tempus. Lorem ipsum dolor sit amet consectetur.
-            Vitae odio quis sit velit dui. Elit risus morbi etiam diam
-            tempus.Lorem ipsum dolor sit amet consectetur.
-            <br></br> Vitae odio quis sit velit dui. Elit risus morbi etiam diam
-            tempus. Lorem ipsum dolor sit amet consectetur. Vitae odio quis sit
-            velit dui. <br></br>
-            Elit risus morbi etiam diam tempus.
-          </motion.p>
-
           <motion.img
             src="/about.png"
             alt="comp"
