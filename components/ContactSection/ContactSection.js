0c62616865af6f9c0444722d24ff77ede9ec0aa1
marcinzygan <soul@o2.pl>
2023-02-16 20:38:23 +0100

    fixed margins and paddings

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -103,27 +103,35 @@ const ContactSection = () => {
           33vw"
           />
         </div>
-
-        <div className={"social__icons" + " " + classes.social__icons_contact}>
-          <Icon icon="entypo-social:facebook-with-circle" />
-          <Icon icon="entypo-social:linkedin-with-circle" />
-          <Icon icon="entypo-social:twitter-with-circle" />
+        <div className={classes.social__details_wrapper}>
+          <div
+            className={"social__icons" + " " + classes.social__icons_contact}
+          >
+            <Icon icon="entypo-social:facebook-with-circle" />
+            <Icon icon="entypo-social:linkedin-with-circle" />
+            <Icon icon="entypo-social:twitter-with-circle" />
+          </div>
+          <p
+            className={
+              classes.contact__details_txt +
+              " " +
+              classes.contact__details_phone
+            }
+          >
+            Telefon<span className="span__accentFirst"> : </span> +48 001 001
+            001
+          </p>
+          <p
+            className={
+              classes.contact__details_txt +
+              " " +
+              classes.contact__details_email
+            }
+          >
+            Email<span className="span__accentFirst"> : </span>
+            comptrends.eu@gmail.com
+          </p>
         </div>
-        <p
-          className={
-            classes.contact__details_txt + " " + classes.contact__details_phone
-          }
-        >
-          Telefon<span className="span__accentFirst"> : </span> +48 001 001 001
-        </p>
-        <p
-          className={
-            classes.contact__details_txt + " " + classes.contact__details_email
-          }
-        >
-          Email<span className="span__accentFirst"> : </span>
-          comptrends.eu@gmail.com
-        </p>
         {/* </div> */}
         <Form />
       </div>
